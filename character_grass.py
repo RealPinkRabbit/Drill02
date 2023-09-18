ad4f014c4bf1ef830093d7bb7c4f194be05ba9be
RealPinkRabbit <sdg0896@gmail.com>
2023-09-18 15:06:33 +0900

    Add Drawing circle

--- character_grass.py
+++ character_grass.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 def square():
     y = 80
@@ -13,7 +14,7 @@ def square():
         grass.draw_now(400,30)
         character.draw_now(x,y)
         y = y + 2
-    while (x >= 40):
+    while (x >= 100):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
@@ -23,11 +24,22 @@ def square():
         grass.draw_now(400,30)
         character.draw_now(x,y)
         y = y - 2
-    while (x <=400):
+    while (x <= 400):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
         x = x + 2
+
+def circle():
+    y = 300
+    x = 400
+    temp = 0
+    while (temp <= 2 * math.pi):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x + 200*math.sin(temp),y + 200*math.cos(temp))
+        temp = temp + 0.1
+        delay(0.05)
         
 open_canvas()
 
@@ -36,5 +48,6 @@ character = load_image('character.png')
 
 while (1):
     square()
+    circle()
 
 close_canvas()
