4afa4f92fed852152549b5a9428f207baee15618
RealPinkRabbit <sdg0896@gmail.com>
2023-09-18 14:57:47 +0900

    Add drawing square

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,40 @@
 from pico2d import *
 
+def square():
+    y = 80
+    x = 400
+    while (x < 700):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        x = x + 2
+    while (y < 500):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        y = y + 2
+    while (x >= 40):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        x = x - 2
+    while (y >= 80):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        y = y - 2
+    while (x <=400):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        x = x + 2
+        
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+while (1):
+    square()
 
 close_canvas()
